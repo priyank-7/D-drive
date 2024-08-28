016b8083208c369b33b1bc55deac7b71409a93fe
priyank patel <priyankpatel9413@gmail.com>
2024-08-28 17:17:43 +0530

    feat: Improve User command Handaling, restructured forward_request flow and Implimented downloade functionality in Client

--- client/src/main/java/com/cloude/Client.java
+++ client/src/main/java/com/cloude/Client.java
@@ -4,7 +4,9 @@ import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.Arrays;
+import java.util.Scanner;
 
+import com.cloude.Commands.UserCommands;
 import com.cloude.headers.Metadata;
 import com.cloude.headers.Request;
 import com.cloude.headers.RequestType;
@@ -15,6 +17,8 @@ public class Client {
     private String loadBalancerHost;
     private int loadBalancerPort;
     private String token;
+    private String destinationPath = "/Users/priyankpatel/Downloads";
+    Scanner scanner;
 
     // TODO: Impliment connection timeout
     // TODO: Impliment TCP Blocking Queue for multiple requests
@@ -22,6 +26,138 @@ public class Client {
     public Client(String loadBalancerHost, int loadBalancerPort) {
         this.loadBalancerHost = loadBalancerHost;
         this.loadBalancerPort = loadBalancerPort;
+        scanner = new Scanner(System.in);
+    }
+
+    public void HandelRequest() {
+        String input;
+        while (true) {
+            System.out.print("Enter command: ");
+            input = scanner.nextLine().trim();
+            String[] parts = input.split(" ", 2);
+            String commandStr = parts[0].toUpperCase();
+            String argument = parts.length > 1 ? parts[1] : "";
+
+            UserCommands command;
+            try {
+                command = UserCommands.valueOf(commandStr);
+            } catch (IllegalArgumentException e) {
+                System.out.println("Unknown command");
+                continue;
+            }
+
+            switch (command) {
+                case AUTH:
+                    if (argument.isEmpty()) {
+                        System.out.println("Requires username:password formate.");
+                    } else {
+                        String[] credentials = argument.split(":");
+                        if (credentials.length != 2) {
+                            System.out.println("Invalid credentials format. Use 'username:password'.");
+                        } else {
+                            if (authenticate(credentials[0], credentials[1])) {
+                                System.out.println("Authenticated successfully.");
+                            } else {
+                                System.out.println("Authentication failed.");
+                            }
+                        }
+                    }
+                    break;
+
+                case PUT:
+                    if (argument.isEmpty()) {
+                        System.out.println("Requires a file path.");
+                    } else {
+                        if (token == null) {
+                            System.out.println("Please authenticate first.");
+                            break;
+                        }
+                        InetSocketAddress nodeAddr = forwardRequest();
+                        if (nodeAddr != null) {
+                            uploadFileToStorageNode(argument, nodeAddr);
+                        }
+                    }
+                    break;
+
+                case GET:
+                    if (argument.isEmpty()) {
+                        System.out.println("GET command requires a file name");
+                    } else {
+                        if (token == null) {
+                            System.out.println("Please authenticate first.");
+                            break;
+                        }
+                        InetSocketAddress nodeAddr = forwardRequest();
+                        if (nodeAddr != null) {
+                            downloadFileFromStorageNode(argument, nodeAddr);
+                        }
+                    }
+                    break;
+
+                case DELETE:
+                    if (argument.isEmpty()) {
+                        System.out.println("DELETE command requires a file path.");
+                    } else {
+                        if (token == null) {
+                            System.out.println("Please authenticate first.");
+                            break;
+                        }
+                        InetSocketAddress nodeAddr = forwardRequest();
+                        if (nodeAddr != null) {
+                            deleteFileFromStorageNode(argument, nodeAddr);
+                        }
+                    }
+                    break;
+
+                case LIST:
+                    if (token == null) {
+                        System.out.println("Please authenticate first.");
+                        break;
+                    }
+                    InetSocketAddress nodeAddr = forwardRequest();
+                    if (nodeAddr != null) {
+                        // listFilesInStorageNode(nodeAddr);
+                    }
+                    break;
+
+                case EXIT:
+                    System.out.println("Exiting the client...");
+                    return;
+
+                default:
+                    System.out.println("Unknown command. Please try again.");
+                    break;
+            }
+        }
+    }
+
+    private InetSocketAddress forwardRequest() {
+        try (Socket socket = new Socket(loadBalancerHost, loadBalancerPort);
+                ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+                ObjectInputStream in = new ObjectInputStream(socket.getInputStream())) {
+
+            // Send a request to forward to the correct storage node
+            Request forwardRequest = Request.builder()
+                    .requestType(RequestType.FORWARD_REQUEST)
+                    .token(this.token)
+                    .build();
+            out.writeObject(forwardRequest);
+            out.flush();
+
+            Response response = (Response) in.readObject();
+            out.writeObject(new Request(RequestType.DISCONNECT));
+            System.out.println("[client]: Got sotage node ip and port: " + response.getPayload().toString());
+
+            if (response.getStatusCode() == StatusCode.SUCCESS) {
+                return (InetSocketAddress) response.getPayload();
+            } else {
+                System.out.println("Failed to forward request: " + response.getPayload());
+                return null;
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public boolean authenticate(String username, String password) {
@@ -52,41 +188,6 @@ public class Client {
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
             return false;
-        }
-    }
-
-    public void uploadFile(String filePath) {
-        if (token == null) {
-            System.out.println("Please authenticate first.");
-            return;
-        }
-
-        try (Socket socket = new Socket(loadBalancerHost, loadBalancerPort);
-                ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-                ObjectInputStream in = new ObjectInputStream(socket.getInputStream())) {
-
-            // Send a request to forward to the correct storage node
-            Request forwardRequest = Request.builder()
-                    .requestType(RequestType.FORWARD_REQUEST)
-                    .token(this.token)
-                    .payload(filePath)
-                    .build();
-            out.writeObject(forwardRequest);
-            out.flush();
-
-            Response response = (Response) in.readObject();
-            out.writeObject(new Request(RequestType.DISCONNECT));
-            System.out.println("[client]: Got sotage node ip and port: " + response.getPayload().toString());
-
-            if (response.getStatusCode() == StatusCode.SUCCESS) {
-                // Now connect directly to the storage node to upload the file
-                uploadFileToStorageNode(filePath, (InetSocketAddress) response.getPayload());
-            } else {
-                System.out.println("Failed to forward request: " + response.getPayload());
-            }
-
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
         }
     }
 
@@ -161,6 +262,73 @@ public class Client {
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    private void downloadFileFromStorageNode(String fileName, InetSocketAddress storageNodeAddress) {
+        try (Socket storageSocket = new Socket(storageNodeAddress.getAddress(), storageNodeAddress.getPort());
+                ObjectOutputStream out = new ObjectOutputStream(storageSocket.getOutputStream());
+                ObjectInputStream in = new ObjectInputStream(storageSocket.getInputStream())) {
+
+            // connectte
+            System.out.println("[Client]: Connected to storage node");
+
+            // Send download file request
+            Request downloadRequest = Request.builder()
+                    .requestType(RequestType.DOWNLOAD_FILE)
+                    .token(token)
+                    .payload(fileName)
+                    .build();
+            out.writeObject(downloadRequest);
+            out.flush();
+
+            // Receive file metadata
+            Response response = (Response) in.readObject();
+            if (response.getStatusCode() != StatusCode.SUCCESS) {
+                System.out.println("Failed to download file: " + response.getPayload());
+                out.writeObject(new Request(RequestType.DISCONNECT));
+                return;
+            }
+
+            Metadata metadata = (Metadata) response.getPayload();
+            System.out.println("[Client]: Metadata received. File size: " + metadata.getSize() + " bytes");
+
+            // Acknowledge metadata receipt
+            Response metadataAckResponse = Response.builder()
+                    .statusCode(StatusCode.SUCCESS)
+                    .build();
+            out.writeObject(metadataAckResponse);
+            out.flush();
+
+            // Receive file data in chunks
+            try (FileOutputStream fos = new FileOutputStream(destinationPath + File.separator + metadata.getName())) {
+                while (true) {
+                    response = (Response) in.readObject();
+
+                    if (response.getStatusCode() == StatusCode.EOF) {
+                        System.out.println("File download complete.");
+                        break;
+                    } else if (response.getStatusCode() == StatusCode.SUCCESS) {
+                        byte[] data = response.getData();
+                        fos.write(data, 0, response.getDataSize());
+                    } else {
+                        System.out.println("File download failed: " + response.getPayload());
+                        break;
+                    }
+                }
+            }
+
+            // Send request to disconnect
+            out.writeObject(new Request(RequestType.DISCONNECT));
+            out.flush();
+            System.out.println("[Client]: Disconnected from storage node");
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void deleteFileFromStorageNode(String fileName, InetSocketAddress storageNodeAddress) {
+
     }
 
 }
--- client/src/main/java/com/cloude/Main.java
+++ client/src/main/java/com/cloude/Main.java
@@ -3,10 +3,6 @@ package com.cloude;
 public class Main {
     public static void main(String[] args) {
         Client client = new Client("localhost", 8080);
-        System.out.println("[Client]: Connecting to Load Balancer");
-        client.authenticate("user", "password");
-        // /Users/priyankpatel/Desktop/test.png
-        System.out.println("[Client]: Uploading file");
-        client.uploadFile("/Users/priyankpatel/Desktop/comp.jpg");
+        client.HandelRequest();
     }
 }
